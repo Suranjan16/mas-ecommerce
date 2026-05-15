2b899a66ce6e2b15234a127b8ab99dd376138b13
Suranjan16 <suranjanmaity851@gmail.com>
2026-05-15 22:56:02 +0530

    feat: add CartItem entity with cart-product relationships and quantity support

---
+++ src/main/java/com/suranjan/mas/cart/entity/CartItem.java
@@ -0,0 +1,50 @@
+package com.suranjan.mas.cart.entity;
+
+import com.suranjan.mas.product.entity.Product;
+import jakarta.persistence.*;
+
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+
+    public CartItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
